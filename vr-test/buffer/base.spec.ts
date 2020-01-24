40bc4e98e408376482b449b29224c20860f093ff
danikaze <danikaze@gmail.com>
2020-01-24 12:49:25 +0900

    Add full-style Tile test

--- vr-test/buffer/base.spec.ts
+++ vr-test/buffer/base.spec.ts
@@ -1,35 +1,69 @@
 import { Buffer } from '@src/buffer';
 import { TestCases } from '@test';
+import { TileStyle } from '@src';
 
 let buffer: Buffer;
+
+function writeText(
+  x: number,
+  y: number,
+  text: string,
+  style?: TileStyle
+): void {
+  let col = x;
+  for (const char of text) {
+    buffer.setTile(col++, y, { ...style, char });
+  }
+}
 
 export const data: TestCases = [
   {
     description: 'Nothing should be rendered until Buffer.render is called',
     test: ({ canvas }) => {
-      let x = 0;
-      const y = 0;
       buffer = new Buffer({
         canvas,
       });
 
-      buffer.setTile(x++, y, { char: 'H' });
-      buffer.setTile(x++, y, { char: 'e' });
-      buffer.setTile(x++, y, { char: 'l' });
-      buffer.setTile(x++, y, { char: 'l' });
-      buffer.setTile(x++, y, { char: 'o' });
-      buffer.setTile(x++, y, { char: ' ' });
-      buffer.setTile(x++, y, { char: 'W' });
-      buffer.setTile(x++, y, { char: 'o' });
-      buffer.setTile(x++, y, { char: 'r' });
-      buffer.setTile(x++, y, { char: 'l' });
-      buffer.setTile(x++, y, { char: 'd' });
-      buffer.setTile(x++, y, { char: '!' });
+      writeText(0, 0, 'Hello world!');
     },
   },
   {
     description: 'Buffer.render() is called, and text should appear',
     test: () => {
+      buffer.render();
+    },
+  },
+  {
+    description: 'Full tile styling test',
+    test: () => {
+      const x = 1;
+      let y = 1;
+      const defaultFont = Buffer.defaultOptions.clearStyle.font;
+
+      // tslint:disable: no-magic-numbers
+      buffer.setTile(buffer.width - 4, y, { bg: 'red' });
+      buffer.setTile(buffer.width - 3, y, { bg: 'green' });
+      buffer.setTile(buffer.width - 2, y, { bg: 'blue' });
+
+      writeText(x, ++y, 'Yellow text', { fg: 'yellow' });
+      writeText(x, ++y, '#6ad080 text', { fg: '#6ad080' });
+      writeText(x, ++y, 'Red background text', { bg: 'red' });
+      writeText(x, ++y, 'offsetX +3 text', { offsetX: 3 });
+      writeText(x, ++y, 'offsetY +10 text', { offsetY: 10 });
+      writeText(5, ++y, 'note how the text rendered', { fg: 'white' });
+      writeText(5, ++y, ' out of its tile is only', { fg: 'white' });
+      writeText(5, ++y, ' overwritten when rendering', { fg: 'white' });
+      writeText(5, ++y, ' over it', { fg: 'white' });
+      writeText(x, ++y, 'Italic text', {
+        font: `italic ${defaultFont}`,
+      });
+      writeText(x, ++y, 'Bold text', {
+        font: `bold ${defaultFont}`,
+      });
+      writeText(x, ++y, 'small-caps text', {
+        font: `small-caps ${defaultFont}`,
+      });
+
       buffer.render();
     },
   },
